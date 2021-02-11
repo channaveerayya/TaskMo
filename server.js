2b73278e4f118c36669852df883a497b0947a4a6
channaveerayya <channucs0009@gmail.com>
2021-02-11 21:51:04 +0530

    code cleanded

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const connectDB = require("./config/db")
 const path = require("path")
+
 const app = express()
 var cors = require("cors")
 //connect Database
 connectDB()
-app.get("/", (req, res) => res.send("API Running..."))
 
 // Init Middleware
 // Extending the limit for uploading profile Image and Resume
@@ -15,6 +15,14 @@ app.use(cors())
 app.use("/api/login", require("./routes/login"))
 app.use("/api/jobs", require("./routes/job"))
 app.use("/api/profile", require("./routes/profile"))
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
